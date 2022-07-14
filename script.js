8b45542eab522068bb20c76d81e2c17c8c74f933
vincentt14 <vincent14@student.umn.ac.id>
2022-07-14 22:54:06 +0700

    com 3
    section account selesai

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+let account = document.querySelector('.user-account');
+
+document.querySelector('#user-btn').onclick = () =>{
+    account.classList.add('active');
+}
+
+document.querySelector('#close-account').onclick = () =>{
+    account.classList.remove('active');
+}
+
 window.onscroll = () =>{
     navbar.classList.remove('active');
 };
