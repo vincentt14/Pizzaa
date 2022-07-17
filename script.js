ab7575288e5740e642e2b3a024b09c05a1451de0
vincentt14 <vincent14@student.umn.ac.id>
2022-07-17 22:23:10 +0700

    com 4

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+// untuk tombol account 
 let account = document.querySelector('.user-account');
 
 document.querySelector('#user-btn').onclick = () =>{
@@ -14,6 +15,45 @@ document.querySelector('#close-account').onclick = () =>{
     account.classList.remove('active');
 }
 
+// untuk tombol orders
+let myOrders = document.querySelector('.my-orders');
+
+document.querySelector('#order-btn').onclick = () =>{
+    myOrders.classList.add('active');
+}
+
+document.querySelector('#close-orders').onclick = () =>{
+    myOrders.classList.remove('active');
+}
+
+// untuk tombol shopping chart
+let chart = document.querySelector('.shopping-chart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    chart.classList.add('active');
+}
+
+document.querySelector('#close-cart').onclick = () =>{
+    chart.classList.remove('active');
+}
+
 window.onscroll = () =>{
     navbar.classList.remove('active');
+    myOrders.classList.remove('active');
+    chart.classList.remove('active');
 };
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
